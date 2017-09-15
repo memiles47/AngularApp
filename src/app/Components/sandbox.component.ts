1d77c71b5fa5aab63fa244810c880df7fdb5128c
Michael E Miles <michael@miles-me.net>
2017-09-15 03:15:16 -0500

    Cleaning for the next section

--- src/app/Components/sandbox.component.ts
+++ src/app/Components/sandbox.component.ts
@@ -9,35 +9,5 @@ import {Customer} from './Customer';
 })
 
 export class SandboxComponent {
-  customer: Customer;
-  customers: Customer[];
 
-  constructor() {
-    this.customer = {
-      id: 1,
-      name: 'John Doe',
-      email: 'john@gmail.com'
-    };
-
-    this.customers = [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'john@gmail.com'
-      },
-      {
-        id: 2,
-        name: 'Brad Traversy',
-        email: 'brad@gmail.com'
-      },
-      {
-        id: 3,
-        name: 'Steve Smith',
-        email: 'ssmith@gmail.com'
-      },
-      {id: 4,
-        name: 'Michael Miles',
-        email: 'michael@miles-me.net'}
-    ];
-  }
 }
