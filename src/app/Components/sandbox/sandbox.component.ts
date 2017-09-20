5435ef182b27d8d390bda0d5ffa1154e3633e83f
Michael E Miles <michael@miles-me.net>
2017-09-20 06:50:12 -0500

    Angular and JavaScript / TypeScript is going to take some getting used to and practice, much like C# and Python

--- src/app/Components/sandbox/sandbox.component.ts
+++ src/app/Components/sandbox/sandbox.component.ts
@@ -8,5 +8,36 @@ import {Component} from '@angular/core';
 })
 
 export class SandboxComponent {
+  people = ['Rick', 'Daryl', 'Carl', 'Glen'];
 
+  people2 = [
+    {
+      firstName: 'Rick',
+      lastName: 'Miles'
+    },
+
+    {
+      firstName: 'Chris',
+      lastName: 'Logue'
+    },
+
+    {
+      firstName: 'Tyra',
+      lastName: 'Winegar'
+    },
+
+    {
+      firstName: 'Mike',
+      lastName: 'Miles'
+    },
+
+    {
+      firstName: 'Diane',
+      lastName: 'Waller'
+    },
+  ];
+
+  constructor() {
+    this.people[2] = 'Carol';
+  }
 }
